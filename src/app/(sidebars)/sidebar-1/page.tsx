97b25fbf2caa673a0c17872535c3ba6394a86018
Salik Larsen <saliklarsen@Saliks-MacBook-Pro-2.local>
2024-10-24 17:07:03 +0200

    sidebar

---
+++ src/app/(sidebars)/sidebar-1/page.tsx
@@ -0,0 +1,12 @@
+import { SidebarProvider } from '@/Components/ui/sidebar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <SidebarProvider>
+      sidebar 1
+    </SidebarProvider>
+  )
+}
+
+export default page
